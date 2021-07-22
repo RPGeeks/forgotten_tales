3acb79605bc505d879f3c13e18ca050da272f04d
catalin407 <ursucataline@yahoo.com>
2021-07-22 23:56:57 +0300

    Made the requested modifications

--- Assets/Scripts/SkyboxController.cs
+++ Assets/Scripts/SkyboxController.cs
@@ -1,16 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Mirror;
 
 public class SkyboxController : MonoBehaviour
 {
     [SerializeField] private float skyboxRotationSpeed = 1f;
-    private Vector2 offset;
       void Update()
     {
-       
-            RenderSettings.skybox.SetFloat("_Rotation", Time.time * skyboxRotationSpeed);
-        
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * skyboxRotationSpeed);
     }
 }
