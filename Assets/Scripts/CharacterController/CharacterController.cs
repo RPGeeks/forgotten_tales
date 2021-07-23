a08c843c01e3508445779682b8bbe9f14384e7a6
Antonio Alexandru Ganea <antonio.ganea@yahoo.com>
2021-07-23 20:11:41 +0300

    Main Character Procedural Animation Core (#14)
    * Initial architecture layout
    * Neated up code
    
    Co-authored-by: ancamariac <ciuciuancamaria@gmail.com>

---
+++ Assets/Scripts/CharacterController/CharacterController.cs
@@ -0,0 +1,45 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : NetworkBehaviour
+{
+    [SerializeField] private HumanoidRigidRig rigParts;
+
+    private Rigidbody rb;
+
+    private CharacterInputFeed cif;
+
+    private ProceduralAnimationController<HumanoidRigidRig> animationController;
+    private MovementController movementController;
+
+    private ProceduralAnimation<HumanoidRigidRig> walkAnim;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        if (isLocalPlayer)
+        {
+            cif = new LocalKeyboardCIF();
+        } else
+        {
+            cif = new EmptyCIF();
+        }
+
+        animationController = new ProceduralAnimationController<HumanoidRigidRig>(cif, rigParts);
+
+        walkAnim = new HumanWalk(rigParts, cif);
+
+        animationController.SwitchTo(walkAnim);
+
+        movementController = new MovementController(rb, cif);
+    }
+
+    private void Update()
+    {
+        animationController.Step(Time.deltaTime);
+        movementController.Step(Time.fixedDeltaTime);
+    }
+}
