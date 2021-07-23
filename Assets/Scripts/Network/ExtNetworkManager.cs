b4a832fdce0a07abb3a39d9e9d06009a8550fc8f
antonioganea <antonio.ganea@yahoo.com>
2021-07-23 21:50:35 +0300

    Camera controller from Cata and ExtNetworkManager

---
+++ Assets/Scripts/Network/ExtNetworkManager.cs
@@ -0,0 +1,17 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtNetworkManager : NetworkManager
+{
+    /*
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        base.OnClientConnect(conn);
+
+        Debug.Log("On client connect!");
+        Debug.Log(conn.clientOwnedObjects.Count);
+    }
+    */
+}
