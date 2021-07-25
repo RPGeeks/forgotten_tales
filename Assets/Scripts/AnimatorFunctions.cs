7b1244e08925b7e210465c0f0441858257a8a86a
CarlosBadoiu <carlos.badoiu@gmail.com>
2021-07-25 12:15:10 +0300

    Am implementat partea de meniu

---
+++ Assets/Scripts/AnimatorFunctions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorFunctions : MonoBehaviour
+{
+	[SerializeField] MenuButtonController menuButtonController;
+	public bool disableOnce;
+
+	void PlaySound(AudioClip whichSound)
+	{
+		if(!disableOnce) {
+			menuButtonController.audioSource.PlayOneShot (whichSound);
+		}
+		else {
+			disableOnce = false;
+		}
+	}
+}	
---
+++ Assets/Scripts/MenuButton.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour
+{
+	[SerializeField] MenuButtonController menuButtonController;
+	[SerializeField] Animator animator;
+	[SerializeField] AnimatorFunctions animatorFunctions;
+	[SerializeField] int thisIndex;
+	public bool isSceneChanger;
+	public int IndexOfSecene;
+	public bool IsPageOpenerBool;
+	public GameObject gm;
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(menuButtonController.index == thisIndex) {
+			animator.SetBool ("selected", true);
+			if(Input.GetAxis ("Submit") == 1) {
+				animator.SetBool ("pressed", true);
+				StartMainMenuy();
+				IsPageOpener();
+			}
+			else if (animator.GetBool ("pressed")) {
+				animator.SetBool ("pressed", false);
+				animatorFunctions.disableOnce = true;
+			}
+		}
+		else {
+			animator.SetBool ("selected", false);
+		}
+    }
+	public void StartMainMenuy()
+	{
+		if (isSceneChanger == true) {
+			SceneManager.LoadScene(IndexOfSecene);
+		} 
+
+	}
+	public void IsPageOpener()
+	{
+		if (IsPageOpenerBool == true) { 
+			gm.SetActive(true);
+		}
+	}
+}
