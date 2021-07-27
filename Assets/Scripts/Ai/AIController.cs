d8c3369ebd716c17d4a6618216f9d271187ff6d5
tiberiu24 <tiberiu.obogeanu@stud.fils.pub.ro>
2021-07-27 23:15:31 +0300

    Ai movement

---
+++ Assets/Scripts/Ai/AIController.cs
@@ -0,0 +1,170 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+[RequireComponent(typeof(NavMeshAgent),typeof(SphereCollider))] 
+public class AIController : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public Transform target;
+
+    public LayerMask Player;
+
+    private SphereCollider sightCollider;
+
+
+    //Patroling
+    [Header("Patroling")] 
+    [SerializeField] private Vector3 walkPoint;
+    [SerializeField] private bool walkPointSet;
+    [SerializeField] private float walkPointRange =5f;
+
+    //Attacking
+    [Header("Attacking")]
+    [SerializeField] private Transform attackPoint;
+    [SerializeField] private int attackValue = 5;
+    //public float attackSphere = 0.5f;
+    [SerializeField] private float timeBetweenAttacks;
+    [SerializeField] private bool alreadyAttacked;
+
+    //States
+    [Header("States")]
+    [SerializeField] private float sightRange =14f;
+    [SerializeField] private float attackRange =5f;
+    [SerializeField] private float playerOutSightRange = 16f;
+    [SerializeField] private bool playerInSightRange;
+    [SerializeField] private bool playerInAttackRange;
+
+    //Weapon fire
+    [SerializeField] private float launchForce;
+    [SerializeField] private Rigidbody projectilePf;
+
+
+
+    private void Start()
+    {
+        //target = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+        sightCollider = GetComponent<SphereCollider>();
+        sightCollider.isTrigger = true;
+        sightCollider.radius = sightRange;
+
+    }
+
+    private void Update()
+    {
+        //playerInSightRange = Physics.CheckSphere(transform.position, sightRange, Player);
+
+        //if (!playerInSightRange && !playerInAttackRange ) { Patroling(); }
+        //if (playerInSightRange && !playerInAttackRange ) { ChasePlayer();}
+        if (target != null) 
+        {
+            float distanceToPlayer = Vector3.Distance(transform.position, target.position);
+            if (distanceToPlayer < attackRange) 
+            { 
+                AttackPlayer(); 
+            }
+            else
+            {
+                ChasePlayer();
+            }
+        }
+
+        else
+        { 
+            Patroling(); 
+        }
+    }
+
+
+    private void Patroling()
+    {
+
+        if (!walkPointSet)
+        { 
+            SearchWalkPoint(); 
+        }
+
+        if (walkPointSet)
+        {
+            agent.SetDestination(walkPoint);
+        }
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //Walkpoint reached
+        if (distanceToWalkPoint.magnitude < 1f)
+        {
+            walkPointSet = false;
+        }
+    }
+
+    private void SearchWalkPoint()
+    {
+        //Calculate random point in range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f))
+        {
+            walkPointSet = true;
+        }
+    }
+
+    private void ChasePlayer()
+    {
+        if (Vector3.Distance(transform.position, target.position) > playerOutSightRange)
+        {
+            target = null;
+            return;
+        }
+        agent.SetDestination(target.position);
+    }
+
+    private void AttackPlayer()
+    {
+       
+        agent.SetDestination(transform.position);
+        transform.LookAt(target);
+        attackPoint = target;
+        if (!alreadyAttacked)
+        {
+            
+            Collider[] hitPlayer = Physics.OverlapSphere(attackPoint.position, attackRange, Player);
+            foreach (Collider Player in hitPlayer)
+            {
+                //Debug.LogWarning("Scad Viata La Player");
+                //player.GetComponent<PlayerHealth>().TakeDamage(attackValue);
+            }
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player") && target == null)
+        {
+            target = other.gameObject.transform;
+            Debug.Log("Player encountered");
+        }    
+    }
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+        //Gizmos.color = Color.green;
+        //Gizmos.DrawWireSphere(transform.position, attackSphere);
+    }
+}
