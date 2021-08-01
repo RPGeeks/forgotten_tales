9f117e10dd5793a3154f3e1cfd100dd53546052b
Menan Yasemin <38699919+yaseminmenan@users.noreply.github.com>
2021-08-01 20:04:36 +0300

    Pet models and prefabs and scripts (#43)
    * Pet models and prefabs and scripts
    * TagManager merge fix
    * Revert "TagManager merge fix"

---
+++ Assets/Scripts/NpcInteraction.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class NpcInteraction : NetworkBehaviour
+{
+    [SerializeField] private GameObject dogPrefab;
+    private GameObject triggeringNpc;
+    private bool triggering;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+       // if (triggering)
+       // {
+          // print("Player is triggering with " + triggeringNpc);
+       // }
+
+        if (isClient && isLocalPlayer)
+        {
+            if (Input.GetKeyDown(KeyCode.G))
+            {
+                CreateDoggy();
+            }
+        }
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "NPC")
+        {
+            triggering = true;
+            triggeringNpc = other.gameObject;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "NPC")
+        {
+            triggering = false;
+            triggeringNpc = null;
+        }
+    }
+
+    [Command]
+    private void CreateDoggy()
+    {
+        GameObject doggy = Instantiate(dogPrefab, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+        doggy.GetComponent<FollowTarget>().SetTarget(transform);
+        NetworkServer.Spawn(doggy);
+    }
+}
