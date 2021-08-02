e53620ea98118e88cb3366880ffb17f4f9bcf1aa
CarlosBadoiu <carlos.badoiu@gmail.com>
2021-08-02 12:32:15 +0300

    menu works now

---
+++ Assets/Scripts/MenuButton.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour
+{
+	[SerializeField] MenuButtonController menuButtonController;
+	[SerializeField] Animator animator;
+	[SerializeField] AnimatorFunctions animatorFunctions;
+	[SerializeField] int thisIndex;
+	public bool isSceneChanger;
+	public int indexOfSecene;
+	public bool isPageOpenerBool;
+	public GameObject gm;
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (menuButtonController.Indexed == thisIndex)
+		{
+			animator.SetBool("selected", true);
+			if (Input.GetAxis("Submit") == 1)
+			{
+				animator.SetBool("pressed", true);
+				StartMainMenuy();
+				PageOpener();
+			}
+			else if (animator.GetBool("pressed"))
+			{
+				animator.SetBool("pressed", false);
+				animatorFunctions.DisabledOnce = true;
+			}
+		}
+		else
+		{
+			animator.SetBool("selected", false);
+		}
+	}
+	public void StartMainMenuy()
+	{
+		if (isSceneChanger == true)
+		{
+			SceneManager.LoadScene(indexOfSecene);
+		}
+	}
+	public void PageOpener()
+	{
+		if (isPageOpenerBool == true)
+		{
+			gm.SetActive(true);
+		}
+	}
+}
