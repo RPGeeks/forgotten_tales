428241c7406d953d90670030226936e5f329757d
Chelcea Claudiu <claudiuchelcea01@gmail.com>
2021-08-14 15:17:37 +0300

    Revent credits scene. Fix sounds bug

--- Assets/Scripts/ButtonSoundsScript.cs
+++ Assets/Scripts/ButtonSoundsScript.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class ButtonSounds : MonoBehaviour
+public class ButtonSoundsScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public AudioSource clickSoundFX;
+	public AudioClip hoverSound;
+	public AudioClip clickSound;
+	
+	public void HoverSound()
+	{
+		clickSoundFX.PlayOneShot(hoverSound);
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public void ClickSound()
+	{
+		clickSoundFX.PlayOneShot(clickSound);
+	}
+
+	private void Update()
+	{
+		if(clickSoundFX.isActiveAndEnabled != true)
+		{
+			clickSoundFX = GameObject.Find("AudioManager").GetComponent<AudioSource>();
+		}
+	}
 }
